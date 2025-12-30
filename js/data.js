c93e957d344718c025874aa1ce3efbb72746c2c0
Marek Dobeš <dobesmarek221@gmail.com>
2025-12-30 21:57:11 +0100

    update

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 export const LS_KEY = "gym_history_v1";
 export const PLAN_KEY = "gym_plan_v1";
-export const APP_VERSION = "1.3.3"; // Version 1.3.3: Chart labels with date ranges
+export const APP_VERSION = "1.3.5"; // Version 1.3.5: Unified text to "Týden"
 
 export const DEFAULT_PLAN = [
     { cat: "ZÁDA", items: ["Přítahy vsedě", "Jednoruční přítahy", "Shrugs", "Deadlift"] },
