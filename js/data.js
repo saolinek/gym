9cc617286d6ab302df463e1e70acc7451b885f39
Marek Dobeš <dobesmarek221@gmail.com>
2026-01-15 17:31:38 +0100

    chore: bump version to 1.6.1

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 export const LS_KEY = "gym_history_v1";
 export const PLAN_KEY = "gym_plan_v1";
 export const SETTINGS_KEY = "gym_settings_v1";
-export const APP_VERSION = "1.6.0"; // Version 1.6.0: History editor - edit past days from settings
+export const APP_VERSION = "1.6.1"; // Version 1.6.1: Fixed history to display only exercises from specific day
 
 export const DEFAULT_PLAN = [
     { cat: "ZÁDA", items: ["Přítahy vsedě", "Jednoruční přítahy", "Shrugs", "Deadlift"] },
